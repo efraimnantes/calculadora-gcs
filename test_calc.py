b26b0f7bb9ce8e95cea42e1233903c31a6c824a1
efraim.nantes <efraim.nantes@ufms.br>
2026-05-25 15:16:29 -0400

    TESTE
    test: adiciona testes do módulo básico

---
+++ test_calc.py
@@ -0,0 +1,24 @@
+import pytest
+
+from calc_basico import somar, subtrair, multiplicar, dividir
+
+
+def test_somar():
+    assert somar(2, 3) == 5
+
+
+def test_subtrair():
+    assert subtrair(10, 4) == 6
+
+
+def test_multiplicar():
+    assert multiplicar(3, 4) == 12
+
+
+def test_dividir():
+    assert dividir(10, 2) == 5
+
+
+def test_dividir_por_zero():
+    with pytest.raises(ValueError):
+        dividir(10, 0)
